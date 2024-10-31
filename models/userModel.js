ea50807517bf4b4e7a0b5b9401c34cb9c15f603a
Janish Nehyan <85396611+Janishnehyan03@users.noreply.github.com>
2024-10-31 14:47:47 +0530

    bioDescription added

--- models/userModel.js
+++ models/userModel.js
@@ -29,6 +29,9 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    bioDescription: {
+      type: String,
+    },
     courses: [
       {
         type: mongoose.Types.ObjectId,
